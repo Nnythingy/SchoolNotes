ae34870fbc1854a5436581fc206a6ad7eb3367e2
Nnythingy <66124098+Nnythingy@users.noreply.github.com>
2021-05-12 23:55:02 +0800

    Delete cs2040 directory

--- cs2040/labs/Week 4 assignment/src/TimeComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-class OtherTimeComparator implements Comparator<Runner> {
-
-    @Override
-    public int compare(Runner r1, Runner r2) {
-        return Double.compare(r1.getOtherLegTime(), r2.getOtherLegTime());
-    }
-}
--- cs2040/lecture programs/Lect1_programs/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-import java.lang.*;  // optional
-
-public class HelloWorld {
-
-  public static void main(String[] args) {
-
-     System.out.println("Hello World!");
-  }
-}
